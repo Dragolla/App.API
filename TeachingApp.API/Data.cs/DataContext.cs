41da564c8810d4e7635b958bddb28a82849acaba
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-04-09 03:01:52 -0700

    added Model and DataContext

--- TeachingApp.API/Data.cs/DataContext.cs
+++ TeachingApp.API/Data.cs/DataContext.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TeachingApp.API.Models;
+
 namespace TeachingApp.API.Data.cs
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        //asdfasdf 
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        public DbSet<Values> Value { get; set; }
+        
     }
 }
--- TeachingApp.API/Models/Value.cs
+++ TeachingApp.API/Models/Value.cs
@@ -2,6 +2,7 @@ namespace TeachingApp.API.Models
 {
     public class Values
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
