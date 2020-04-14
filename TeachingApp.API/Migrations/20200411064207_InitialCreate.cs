21c71ed3660442fe081f912d92302c742215e518
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-04-14 00:27:39 -0700

    created database

--- TeachingApp.API/Migrations/20200411064207_InitialCreate.cs
+++ TeachingApp.API/Migrations/20200411064207_InitialCreate.cs
@@ -2,7 +2,7 @@
 
 namespace TeachingApp.API.Migrations
 {
-    public partial class InitialCreate : Migration
+    public partial class InitialCreate : Migration  
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
