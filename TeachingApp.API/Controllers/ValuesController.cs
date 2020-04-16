aa43434c2f12489eab138d35bb2153faae1d346f
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-04-16 00:07:22 -0700

    made some changes to database and values controller

--- TeachingApp.API/Controllers/ValuesController.cs
+++ TeachingApp.API/Controllers/ValuesController.cs
@@ -11,22 +11,26 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
         public ValuesController(DataContext context)
         {
-            
+            _context = context;
+
         }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+           var values = _context.Values.ToList();
+           return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = _context.Values.FirstOrDefault(x=> x.ID == id);
+            return Ok(value);
         }
 
         // POST api/values
