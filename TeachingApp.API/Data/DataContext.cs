2ca2faf8c1fb84715e565c62b376cbba7d5196bb
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-04-27 23:55:16 -0700

    started working on user

--- TeachingApp.API/Data/DataContext.cs
+++ TeachingApp.API/Data/DataContext.cs
@@ -7,7 +7,7 @@ namespace TeachingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
         public DbSet<Value> Values { get; set; }
-
+        public DbSet<User> Users { get; set; }
         
     }
 }
