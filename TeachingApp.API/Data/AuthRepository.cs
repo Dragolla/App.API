c196d79a0e4edc02e6b47c4a5b0141e4f229a5fa
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-04-30 02:21:15 -0700

    final small changes

--- TeachingApp.API/Data/AuthRepository.cs
+++ TeachingApp.API/Data/AuthRepository.cs
@@ -21,12 +21,16 @@ namespace TeachingApp.API.Data
             throw new System.NotImplementedException();
         }
 
-        public Task<User> Register(User user, string password)
+        public async Task<User> Register(User user, string password)
         {
             byte[] passwordHash, passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
          user.PasswordHash = passwordHash;
          user.PasswordSalt = passwordSalt;
+         await _context.Users.AddAsync(user);
+         await _context.SaveChangesAsync();
+    
+            return user;
 
         }
 
