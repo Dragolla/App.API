d24302f0a670675816b66c84be356da8f4455d8e
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-05-04 23:48:46 -0700

    small change

--- TeachingApp.API/Dtos/UserForRegisterDto.cs
+++ TeachingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations; 
 namespace TeachingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required] 
         public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set;}
 }
 }
