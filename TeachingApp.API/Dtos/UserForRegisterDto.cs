72dbc270b09aaac894348ce9e58f68000dda9fe0
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-05-04 23:57:14 -0700

    Small change

--- TeachingApp.API/Dtos/UserForRegisterDto.cs
+++ TeachingApp.API/Dtos/UserForRegisterDto.cs
@@ -6,7 +6,7 @@ namespace TeachingApp.API.Dtos
         [Required] 
         public string Username { get; set; }
         [Required]
-        [StringLength(8, MinimumLength = 4)]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set;}
 }
 }
