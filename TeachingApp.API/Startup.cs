1d01ff365323162ff03dbbd955d6d2ed4629faa8
_Kevin_Zhang_ <49049813+Dragolla@users.noreply.github.com>
2020-05-28 00:19:08 -0700

    Small change on global error handling

--- TeachingApp.API/Startup.cs
+++ TeachingApp.API/Startup.cs
@@ -4,14 +4,14 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using TeachingApp.API.Data;
-
-
+using TeachingApp.API.Helpers;
 
 namespace TeachingApp.API
 {
@@ -53,7 +53,11 @@ namespace TeachingApp.API
                     builder.Run(async context => {
                         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                         var error = context.Features.Get<IExceptionHandlerFeature>();
-                    })
+                        if(error != null){
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
                 });
             }
 
